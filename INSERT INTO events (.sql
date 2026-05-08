2e6d7e23c96137d94a86e68aca00174db425152b
nurrr11 <adilaaida643@gmail.com>
2026-05-08 11:28:05 +0800

    Add files via upload

---
+++ INSERT INTO events (.sql
@@ -0,0 +1,20 @@
+INSERT INTO events (
+    id,
+    organizer_id,
+    title,
+    description,
+    date,
+    location,
+    slots,
+    registered
+  )
+VALUES (
+    14,
+    2,
+    'Mayday',
+    'Animal Care Event.',
+    '2026-06-20',
+    'Econia Garden',
+    10,
+    0
+  );
---
+++ MySQL Local.session.sql
@@ -0,0 +1,4 @@
+SELECT * FROM deepseek_db.users;
+
+INSERT INTO users (name, email, password, role, is_approved) VALUES
+('Admin User', 'adminmaya@gmail.com', 'adminMaya', 'admin', TRUE);
