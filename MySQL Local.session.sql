bec038b1c36fd46ce1fcd022feae4aaeb6d7457c
! nebulara. <adilaaida643@gmail.com>
2026-07-06 12:00:32 +0800

    Add files via upload

--- MySQL Local.session.sql
+++ MySQL Local.session.sql
@@ -2,3 +2,23 @@ SELECT * FROM deepseek_db.users;
 
 INSERT INTO users (name, email, password, role, is_approved) VALUES
 ('Admin User', 'adminmaya@gmail.com', 'adminMaya', 'admin', TRUE);
+INSERT INTO gratuity (
+    Gratuity_ID,
+    Event_ID,
+    Volunteer_ID,
+    Student_ID,
+    Gratuity_Date,
+    Gratuity_Method,
+    Gratuity_Amount,
+    Gratuity_Status
+  )
+VALUES (
+    Gratuity_ID:int,
+    Event_ID:int,
+    Volunteer_ID:int,
+    'Student_ID:varchar',
+    'Gratuity_Date:date',
+    'Gratuity_Method:enum',
+    'Gratuity_Amount:decimal',
+    'Gratuity_Status:enum'
+  );
